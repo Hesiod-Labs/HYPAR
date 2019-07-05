7df028ecbf0d4a20383aefe931b5a2671f27c410
Ryan Tatton <rdt46@case.edu>
2019-07-05 19:31:14 -0400

    Should fix the pyEX 0.1.14 bug.

---
+++ hypar/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='hypar',
+    version='0.1-dev',
+    packages=[''],
+    url='https://github.com/Hesiod-Labs/HYPAR',
+    license='',
+    author='hLabs',
+    author_email='',
+    description='',
+    install_requires=['pyEX==0.1.13']
+)
